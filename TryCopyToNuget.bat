645bac93bd997413b0778f4fbe483b6e4725cf47
xuyuwtu <3082068984@qq.com>
2025-07-19 13:47:49 +0800

    feat: 修改复制脚本

--- TryCopyToNuget.bat
+++ TryCopyToNuget.bat
@@ -1,2 +1,10 @@
-if exist "%USERPROFILE%\.nuget\packages\otapi.upcoming\3.1.20\lib\net6.0\OTAPI.dll" copy /y OTAPI\OTAPI.xml "%USERPROFILE%\.nuget\packages\otapi.upcoming\3.1.20\lib\net6.0\"
-pause
+# 2>nul&cls&@echo off& powershell -Command "& {Invoke-Expression $([System.IO.File]::ReadAllText('%~0', [System.Text.Encoding]::UTF8))}" & goto :EOF
+if(Test-Path "${env:USERPROFILE}\.nuget\packages\otapi.upcoming\") {
+    $minVersion = [version]::new(3, 1, 20);
+    $ver = [version]::new()
+    foreach($version in (Get-ChildItem -Attributes Directory "${env:USERPROFILE}\.nuget\packages\otapi.upcoming\")) {
+        if([version]::TryParse($version.Name, ([ref]$ver)) -and ($ver -ge $minVersion)) {
+            Copy-Item 'OTAPI\OTAPI.xml' $version.FullName 
+        }
+    }
+}
